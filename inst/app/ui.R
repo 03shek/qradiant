18a04362e6fc88ec6422fe16c400e7667f5c19b4
Tina <tina.shek@uhn.ca>
2024-03-14 17:17:39 -0400

    added comments

--- inst/app/ui.R
+++ inst/app/ui.R
@@ -4,7 +4,7 @@ navbar_proj(
     do.call(
       navbarPage,
       c(
-        "Radiant for R",
+        "qRadiant for R",
         getOption("radiant.nav_ui"),
         getOption("radiant.design_ui"),
         getOption("radiant.basics_ui"),

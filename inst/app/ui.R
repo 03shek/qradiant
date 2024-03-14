2a09aa9bef8de0423b8e89a824c57d584090da46
Tina <tina.shek@uhn.ca>
2024-03-14 17:18:52 -0400

    More comments

--- inst/app/ui.R
+++ inst/app/ui.R
@@ -4,13 +4,26 @@ navbar_proj(
     do.call(
       navbarPage,
       c(
-        "qRadiant for R",
+        "QIPCM Radiant for R",
+        # Data tab
         getOption("radiant.nav_ui"),
+
+        # Design tab
         getOption("radiant.design_ui"),
+
+        # Basics tab
         getOption("radiant.basics_ui"),
+
+        # Model tab
         getOption("radiant.model_ui"),
+
+        # Multivariate tab
         getOption("radiant.multivariate_ui"),
+
+        # Reporting tab
         getOption("radiant.shared_ui"),
+
+        # Help tab
         help_menu("help_ui")
       )
     )
